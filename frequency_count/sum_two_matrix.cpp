6cf860d6992fc1069b405efd97ddac23d372bc9a
user-root <usersolo1@protonmail.com>
2026-07-23 00:26:31 +0000

    Add the analizy of time and space

--- frequency_count/sum_two_matrix.cpp
+++ frequency_count/sum_two_matrix.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
+/* Analize:
+ * Time: O(n^2)
+ * Space: O(n^2)
+ * */
+
 int (*add(int matrix_a[][3], int matrix_b[][3]))[3] {
   static int matrix_c[3][3];
 
-  for (int i = 0; i < 3; i++) {
-    for (int u = 0; u < 3; u++) {
-      matrix_c[i][u] = matrix_a[i][u] + matrix_b[i][u];
+  for (int i = 0; i < 3; i++) {                         // n+1
+    for (int u = 0; u < 3; u++) {                       // n(n+1) = n^2+n
+      matrix_c[i][u] = matrix_a[i][u] + matrix_b[i][u]; // nxn
     }
   }
+  // f(n) = 2n^2+2n+1
   return matrix_c;
 }
 
